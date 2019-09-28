4f39fa173abce8c1c09d4c5a9303efc6513559e0
tackgnol <koscielniakadam@gmail.com>
2019-09-28 13:31:50 +0200

    commiting to store the changes

---
+++ src/Resources/events/SessionOne/index.js
@@ -0,0 +1,11 @@
+const globalTestsJSON = require('./GlobalTest.json.js');
+const statInsightsJSON = require('./StatInsights.json.js');
+const messageMultiplePlayersJSON = require('./MessageMultiplePlayers.json.js');
+const multiMessageJSON = require('./MultiMessage.json.js');
+
+module.exports = {
+	globalTests: globalTestsJSON,
+	statInsights: statInsightsJSON,
+	messageMultiplePlayers: messageMultiplePlayersJSON,
+	multiMessage: multiMessageJSON,
+};
