90b05dee59135c46b67d4c6bbc79d073f801de32
Adam Kościelniak <koscielniakadam@gmail.com>
2021-07-29 11:22:58 +0200

    Removing GraphQL it is no longer needed and does not fit the 'direct to DB approach I wish to take'.

--- src/GraphQL/Mutations/REMOVE_PLAYER.ts
+++
@@ -1,9 +0,0 @@
-import gql from 'graphql-tag';
-
-const REMOVE_PLAYER = gql`
-	mutation RemovePlayer($inputPlayerId: String, $inputGame: String) {
-		removePlayer(inputPlayerId: $inputPlayerId, inputGame: $inputGame)
-	}
-`;
-
-export default REMOVE_PLAYER;
